76bbf4459c4cc36f4a46eb24ef5e883b4133161d
Agustin Lavarello <agustinlare@gmail.com>
2023-04-30 18:27:45 -0300

    pepito

--- main.go
+++ main.go
@@ -1,105 +1,43 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"net/url"
 	"os"
-	"strings"
-	"time"
-
-	client "github.com/influxdata/influxdb1-client/v2"
 )
 
 func main() {
-	res, err := http.Get(os.Getenv("ENDPOINT_URL"))
+	// Replace with your webhook URL
+	webhookURL := os.Getenv("DISCORD_WEBHOOK_URL")
+
+	// Create a new Embed object
+	embed := struct {
+		URL   string `json:"url"`
+		Title string `json:"title"`
+	}{
+		URL:   "https://dolarhoy.com/i/cotizaciones/dolar-blue",
+		Title: "Dolar Hoy Blue",
+	}
+
+	// Marshal the Embed object to JSON
+	payload, err := json.Marshal(embed)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	defer res.Body.Close()
-
-	decoder := json.NewDecoder(res.Body)
-	var data map[string]interface{}
-	_ = decoder.Decode(&data)
-
-	for _, v := range data {
-		dolar, _ := v.(map[string]interface{})
-		message := fmt.Sprintf("%s %s, Compra: %s, Venta: %s", dolar["name"], dolar["date"], dolar["buy"], dolar["sell"])
-
-		sendNotification(os.Getenv("WEBHOOK_URL"), message)
-
-		influxdb, exists := os.LookupEnv("INFLUXDB_URL")
-
-		if exists && strings.Contains(dolar["name"].(string), "blue") {
-
-			err := writeToInfluxDB(dolar["sell"].(string), dolar["buy"].(string), influxdb)
-			if err != nil {
-				fmt.Println("Error writing to InfluxDB:", err)
-			}
-		}
-	}
-
-	os.Exit(0)
-}
-
-func writeToInfluxDB(sell string, buy string, influxdb string) error {
-	c, err := client.NewHTTPClient(client.HTTPConfig{
-		Addr: influxdb,
-	})
+	// Create a new POST request to the webhook URL with the JSON payload
+	req, err := http.NewRequest("POST", webhookURL, bytes.NewBuffer(payload))
 	if err != nil {
-		return err
+		panic(err)
 	}
-	defer c.Close()
+	req.Header.Set("Content-Type", "application/json")
 
-	bp, err := client.NewBatchPoints(client.BatchPointsConfig{
-		Database:  "dolar",
-		Precision: "s",
-	})
+	// Send the request
+	client := &http.Client{}
+	resp, err := client.Do(req)
 	if err != nil {
-		return err
-	}
-
-	tags := map[string]string{"currency": "USD"}
-	fields := map[string]interface{}{
-		"sell": sell,
-		"buy":  buy,
-	}
-	p, err := client.NewPoint("exchange_rate", tags, fields, time.Now())
-	if err != nil {
-		return err
-	}
-
-	bp.AddPoint(p)
-
-	if err := c.Write(bp); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func sendNotification(webhookUrl, message string) {
-	var field string = "text"
-
-	if strings.Contains(webhookUrl, "discord") {
-		field = "content"
-	}
-
-	formData := url.Values{
-		field: {message},
-	}
-	resp, err := http.PostForm(webhookUrl, formData)
-	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(string(body))
 }
